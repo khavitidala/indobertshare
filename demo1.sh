55a5efa3d14ff09d7d9b93d184df14c33397f947
Ryan Abdurohman <ryanabdurohman81@gmail.com>
2023-02-07 21:55:42 +0700

    fix demo and requirement

--- demo1.sh
+++ demo1.sh
@@ -6,7 +6,6 @@ pip install -r requirements.txt
 # download liputan6_data
 cd summarization
 gdown --id 1LNRkeSVkTpS7-o8Go7vk6YZNVZ1eHZFh
-tar -xvf liputan6_data.tar.gz
 
 # download summarization indolem-bertshare 150000 checkpoint
 gdown --id 1Q9_LLhPhbboOPQ-u8aw8dzFYIMtijXKC
@@ -16,5 +15,3 @@ tar -xvf checkpoint-150000.tar.gz
 cd ..
 cd paraphrase
 gdown --id 1XyRIW4O0T4GeglufIpRobpP3hX1nbFNd
-cd save
-tar -xvf filtered_paracotta.tar.gz
---
+++ demo2.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# install all requirements
+pip install -r requirements.txt
+
+# download liputan6_data
+cd summarization
+tar -xvf liputan6_data.tar.gz
+
+# download summarization indolem-bertshare 150000 checkpoint
+tar -xvf checkpoint-150000.tar.gz
+
+# download indobart fine-tuned paraphrase generation checkpoint
+cd ..
+cd paraphrase
+cd save
+tar -xvf filtered_paracotta.tar.gz
